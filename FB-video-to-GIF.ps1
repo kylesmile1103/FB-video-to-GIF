de01a215691e81c525f25baeb4f628fce629bf3f
Kyle Pham <mrquangkhai98@set.edu.vn>
2021-06-11 03:11:37 +0700

    add screenshot, fix package install

--- FB-video-to-GIF.ps1
+++ FB-video-to-GIF.ps1
@@ -1,6 +1,3 @@
-$scriptPath = $myinvocation.MyCommand.Definition
-$localFile = split-path -parent $scriptPath
-
 Add-Type -AssemblyName PresentationFramework | Out-Null
 Add-Type -AssemblyName System.Drawing | Out-Null
 Add-Type -AssemblyName System.Windows.Forms | Out-Null
@@ -52,7 +49,7 @@ $wshell = New-Object -ComObject Wscript.Shell
                     </Canvas>
                 </Viewbox>
                 <Label FontWeight="bold" Grid.Column="1" Content="Youtube-dl Package" />
-                <Button Grid.Column="2" Width="90" Height="28" Content="Install" x:Name="insYoutubeDlBtn"/>
+                <Button Grid.Column="2" Width="80" Height="28" Content="Install" x:Name="insYoutubeDlBtn"/>
             </Grid>
         </Grid>
         <Grid Grid.Row="1">
@@ -217,19 +214,46 @@ function ffmpegProcess ([string]$inputs, [string]$opt, [string]$output) {
     }
     # $wshell.Popup("$ffmpegCMD", 0, "DEBUG")
 }
-
+$warnBadge = "M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z"
+$checkBadge = "M10,17L5,12L6.41,10.58L10,14.17L17.59,6.58L19,8M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z"
+if (!(. checkCommand "ffmpeg")) {
+    $ffmpeg.fill = "Red"
+    $ffmpeg.Data = $warnBadge
+}
+else {
+    $insFFmpegBtn.Content = "Reinstall"
+}
+if (!(. checkCommand "youtube-dl")) {
+    $youtubedl.fill = "Red"
+    $youtubedl.Data = $warnBadge
+}
+else {
+    $insYoutubeDlBtn.Content = "Reinstall"
+}
 
 $insFFmpegBtn.Add_Click( {
-        Install-ChocoPkg "ffmpeg"
-        if (. checkCommand "ffmpeg") {
-            $this.Content = "Reinstall"
+        if (checkAdminPrivileges -ne $false) {
+            Install-ChocoPkg "ffmpeg"
+            if (. checkCommand "ffmpeg") {
+                $this.Content = "Reinstall"
+                $ffmpeg.Data = $checkBadge
+            }
+        }
+        else {
+            $wshell.Popup("Run as Administrator privileges to install these packages!", 0, "ERROR")
         }
     })
 
 $insYoutubeDlBtn.Add_Click( {
-        Install-ChocoPkg "youtube-dl"
-        if (. checkCommand "youtube-dl") {
-            $this.Content = "Reinstall"
+        if (checkAdminPrivileges -ne $false) {
+            Install-ChocoPkg "youtube-dl"
+            if (. checkCommand "youtube-dl") {
+                $this.Content = "Reinstall"
+                $youtubedl.Data = $checkBadge
+            }
+        }
+        else {
+            $wshell.Popup("Run as Administrator privileges to install these packages!", 0, "ERROR")
         }
     })
 
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # FB-video-to-GIF
+A Powershell GUI helps accessing to WI-FI Credentials on Windows with ease.
+
+## Usage
+
+### Option 1
+* Step 1: Clone this repo or just download the app.ps1
+* Step 2: Make sure your Execution Policy is not `Restricted`, you can check by issuing this command:  
+  ```Get-ExecutionPolicy```. Otherwise, run Powershell as Administrator and execute  
+  ```Set-ExecutionPolicy unrestricted```.
+* Step 3: Right click at the app.ps1, hit `Run with Powershell` and you'll be good to go.
+
+### Option 2
+* Open Powershell then execute these following commands (copy all and paste by pressing right click onto the Powershell console):
+
+```powershell
+saps powershell -Verb RunAs -ArgumentList ('-Command "{0}"' -f $(icm -ScriptBlock $([scriptblock]::create( {
+$uri='https://raw.githubusercontent.com/kylesmile1103/Get-Wifi-Credentials/main/app.ps1';
+Set-ExecutionPolicy Unrestricted -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString($uri));
+})))) -WindowStyle hidden
+
+```
+
+## Screenshot
+![alt text](https://github.com/kylesmile1103/Get-Wifi-Credentials/blob/main/screenshot.png "Screenshot")
